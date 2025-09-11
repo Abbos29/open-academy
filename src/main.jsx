64673cb3cb9af49aae8eee9646e82cb47df143b3
Abbos29 <abbosvaliev2003@gmail.com>
2025-09-11 12:17:58 +0500

    fix: link to it academy website

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.scss'
 import App from './App.jsx'
 
+// проверка текущего URL
+if (window.location.hostname === "open-academy.netlify.app") {
+  window.location.replace("https://it-academy.uz/openday");
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
